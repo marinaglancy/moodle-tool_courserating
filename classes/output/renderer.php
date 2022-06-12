488d1a4a59a36b707baae539ace6b6744bdda987
Marina Glancy <marina@moodle.com>
2022-06-12 20:19:56 +0200

    Move the widget to the header area

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -20,6 +20,7 @@ use plugin_renderer_base;
 use tool_courserating\external\summary_exporter;
 use tool_courserating\external\ratings_list_exporter;
 use tool_courserating\helper;
+use tool_courserating\local\models\summary;
 use tool_courserating\permission;
 
 /**
@@ -38,14 +39,9 @@ class renderer extends plugin_renderer_base {
      * @return string
      */
     public function cfield(int $courseid): string {
-        $content = '';
-        $data = helper::get_course_rating_data_in_cfield($courseid);
-        if ($data) {
-            $output = $this->page->get_renderer('core_customfield');
-            $fd = new \core_customfield\output\field_data($data);
-            $content .= $output->render($fd);
-        }
-        return $content;
+        $summary = summary::get_for_course($courseid);
+        $data = (new summary_exporter(0, $summary))->export($this);
+        return $this->render_from_template('tool_courserating/summary_for_cfield', $data);
     }
 
     /**
